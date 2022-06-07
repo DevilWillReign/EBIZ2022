bf7efe3aeb44e7c9eede8d24e206e4e8ead00fdf
P S <ps0hakai.tenshi@gmail.com>
2022-06-07 11:51:48 +0200

    Project - docker update

--- frontend/src/util/api.js
+++ frontend/src/util/api.js
@@ -1,11 +1,11 @@
 import axios from "axios"
 
 const API = () => axios.create({
-    baseURL: process.env.API_BASE_URL || "http://localhost:9000/api/v1"
+    baseURL: process.env.REACT_APP_API_BASE_URL || "http://localhost:9000/api/v1"
 })
 
 const API_PROTECTED = () => axios.create({
-    baseURL: process.env.API_BASE_URL || "http://localhost:9000/api/v1",
+    baseURL: process.env.REACT_APP_API_BASE_URL || "http://localhost:9000/api/v1",
     withCredentials: true
 })
 
